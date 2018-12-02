12b812dc3fc78590ae2128611a95fb3516345ffb
julemandennb <bo.naerumhansen@gmail.com>
2018-12-02 19:09:36 +0100

    get play to play on new output

--- HotKey/Form1.cs
+++ HotKey/Form1.cs
@@ -16,9 +16,14 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
+        private musicPalyClass musicPalyClass;
+
         public Form1()
         {
             InitializeComponent();
+
+            this.musicPalyClass = new musicPalyClass();
         }
 
         bool isRunning = true;
@@ -27,24 +32,39 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+            //start check key
             TH = new Thread(Keyboardd);
             TH.SetApartmentState(ApartmentState.STA);
             TH.Start();
         }
 
+        /// <summary>
+        /// check key 
+        /// </summary>
         void Keyboardd()
         {
+            
             while(isRunning)
             {
                 Thread.Sleep(40);
-                if((Keyboard.GetKeyStates(Key.A) & KeyStates.Down) > 0 && (Keyboard.GetKeyStates(Key.S) & KeyStates.Down) > 0)
+                if((Keyboard.GetKeyStates(Key.E) & KeyStates.Down) > 0 && (Keyboard.GetKeyStates(Key.X) & KeyStates.Down) > 0)
                 {
-                    new musicPalyClass().Play();
+                    this.musicPalyClass.Play();
+                }
+                else if((Keyboard.GetKeyStates(Key.W) & KeyStates.Down) > 0 && (Keyboard.GetKeyStates(Key.E) & KeyStates.Down) > 0 && (Keyboard.GetKeyStates(Key.B) & KeyStates.Down) > 0)
+                {
+                    System.Diagnostics.Process.Start("http://google.com");
                 }
 
             }
         }
 
+        /// <summary>
+        /// if close app end key check
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             isRunning = false;
@@ -52,6 +72,11 @@ namespace WindowsFormsApp1
             TH.Abort();
         }
 
+        /// <summary>
+        /// to set on Pause or Start ket check
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void buttonPauseOrStart_Click(object sender, EventArgs e)
         {
             if(isRunning == true)
--- musicPlayClass/musicPalyClass.cs
+++ musicPlayClass/musicPalyClass.cs
@@ -18,25 +18,65 @@ namespace musicPlayClass
 
         }
 
-        NAudio.Wave.WaveIn sourceStream = null;
-        NAudio.Wave.DirectSoundOut waveOut = null;
+
+        WaveOut output = null;
 
         public void Play()
         {
             string assemblyPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\Resource\sounder\explosion.mp3";
 
 
-                AudioFileReader audio = new AudioFileReader(assemblyPath);
-
-                IWavePlayer player = new WaveOut(WaveCallbackInfo.FunctionCallback());
-                player.Init(audio);
-                player.Play();
+            string _outPath_ = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\Resource\sounder\explosion.wav";
 
 
+            if (!File.Exists(_outPath_))
+            {
 
+                using (Mp3FileReader mp3 = new Mp3FileReader(assemblyPath))
+                {
+                    using (WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(mp3))
+                    {
+                        WaveFileWriter.CreateWaveFile(_outPath_, pcm);
+                    }
+                }
+            }
+        
+            // 0 == defulet
+            playSound(0, _outPath_);
 
+        }
 
-            
+        /// <summary>
+        /// play sound 
+        /// </summary>
+        /// <param name="deviceNumber">device to play on</param>
+        /// <param name="path">path to file</param>
+        public void playSound(int deviceNumber , string path)
+        {
+            this.disposeWave();
+
+            var waveReader = new NAudio.Wave.WaveFileReader(path);
+            var waveOut = new NAudio.Wave.WaveOut();
+            waveOut.DeviceNumber = deviceNumber;
+            output = waveOut;
+            output.Init(waveReader);
+            output.Play();
+        }
+
+        /// <summary>
+        /// end sound 
+        /// </summary>
+        public void disposeWave()
+        {
+            if (output != null)
+            {
+                if (output.PlaybackState == NAudio.Wave.PlaybackState.Playing)
+                {
+                    output.Stop();
+                    output.Dispose();
+                    output = null;
+                }
+            }
 
         }
 
