ef41a8a0bc4cb639aa9853b133d21016ef24448d
julemandennb <bo.naerumhansen@gmail.com>
2018-12-02 14:26:11 +0100

    add code

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPauseOrStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonPauseOrStart
+            // 
+            this.buttonPauseOrStart.Location = new System.Drawing.Point(30, 25);
+            this.buttonPauseOrStart.Name = "buttonPauseOrStart";
+            this.buttonPauseOrStart.Size = new System.Drawing.Size(131, 102);
+            this.buttonPauseOrStart.TabIndex = 0;
+            this.buttonPauseOrStart.Text = "Pause";
+            this.buttonPauseOrStart.UseVisualStyleBackColor = true;
+            this.buttonPauseOrStart.Click += new System.EventHandler(this.buttonPauseOrStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 147);
+            this.Controls.Add(this.buttonPauseOrStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPauseOrStart;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Threading;
+
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool isRunning = true;
+
+        public Thread TH;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TH = new Thread(Keyboardd);
+            TH.SetApartmentState(ApartmentState.STA);
+            TH.Start();
+        }
+
+        void Keyboardd()
+        {
+            while(isRunning)
+            {
+                Thread.Sleep(40);
+                if((Keyboard.GetKeyStates(Key.A) & KeyStates.Down) > 0 && (Keyboard.GetKeyStates(Key.S) & KeyStates.Down) > 0)
+                {
+
+                }
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            isRunning = false;
+
+            TH.Abort();
+        }
+
+        private void buttonPauseOrStart_Click(object sender, EventArgs e)
+        {
+            if(isRunning == true)
+            {
+                isRunning = false ;
+                buttonPauseOrStart.Text = "Start";
+
+                TH.Abort();
+
+            }
+            else
+            {
+                isRunning = true;
+                buttonPauseOrStart.Text = "Pause";
+
+                TH = new Thread(Keyboardd);
+                TH.SetApartmentState(ApartmentState.STA);
+                TH.Start();
+            }
+        }
+    }
+}
